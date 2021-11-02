1e70e69cc514d7619100dbe46239f80d89983a25
Jason Mendes <jason_mendes_1998@hotmail.com>
2021-11-02 19:01:36 +0000

    feat: rbac

---
+++ console/migrations/m211102_182214_init_rbac.php
@@ -0,0 +1,62 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m211102_182214_init_rbac
+ */
+class m211102_182214_init_rbac extends Migration
+{
+   
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+        $auth = Yii::$app->authManager;
+
+        // adiciona a permissão "createPost"
+        $CreateEmpresa = $auth->createPermission('createEmpresa');
+        $CreateEmpresa->description = 'Criar uma empresa';
+        $auth->add($CreateEmpresa);
+
+        // adiciona a permissão "updatePost"
+        $updateEmpresa = $auth->createPermission('updateEmpresa');
+        $updateEmpresa->description = 'Update empresa';
+        $auth->add($updateEmpresa);
+        
+        // adiciona a permissão  "deleteCargo"
+        $deleteEmpresa = $auth->createPermission('deleteEmpresa');
+        $deleteEmpresa->description = 'Delete Empresa';
+        $auth->add($deleteEmpresa);
+
+        // add "author" role and give this role the "createPost" permission
+        $author = $auth->createRole('author');
+        $auth->add($author);
+        $auth->addChild($author, $CreateEmpresa);
+
+        // add "admin" role and give this role the "updatePost" permission
+        // as well as the permissions of the "author" role
+        $admin = $auth->createRole('admin');
+        $contratante = $auth->createRole('contratante');
+        $prestador = $auth->createRole('prestador');
+        $guest = $auth->createRole('guest');
+        $auth->add($admin);
+        $auth->add($contratante);
+        $auth->add($guest);
+        $auth->add($prestador);
+        $auth->addChild($contratante, $updateEmpresa);
+        $auth->addChild($admin, $author);
+
+        // Assign roles to users. 1 and 2 are IDs returned by IdentityInterface::getId()
+        // usually implemented in your User model.
+        $auth->assign($author, 2);
+        $auth->assign($admin, 1);
+    }
+
+    public function down()
+    {
+        $auth = Yii::$app->authManager;
+
+        $auth->removeAll();
+    }
+    
+}
